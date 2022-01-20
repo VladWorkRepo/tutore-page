34af9513f6448c43a8cf2aa47521638ed807d892
Vlad <vlad.herasymovych@tutore.eu>
2022-01-20 15:35:39 +0100

    header basic elements

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav>
+        <v-app-bar
+        flat
+        app
+        >
+        <v-row style="height: 64px" class="d-flex justify-space-around">
+            <v-col  lg="2">
+                <img width="200" src="../../assets/logo/main_logo.webp" alt="Tutore">
+            </v-col>
+            <v-col style="height: 64px" lg="3" class="d-flex justify-space-around align-center">
+                <div>
+                    <v-icon color="#813188">facebook</v-icon>
+                    <v-icon color="#813188">facebook</v-icon>
+                    <v-icon color="#813188">facebook</v-icon>
+                </div>
+                <v-btn elevation="2" dark outlined color="#813188">Strona firmowa</v-btn>
+            </v-col>
+            <v-col class="pa-1" lg="1">
+                <v-select style="width: 100px" label="Polski" outlined></v-select>
+            </v-col>
+        </v-row>
+        </v-app-bar>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
